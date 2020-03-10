6e00bbd023d5a17b28ce3c3b6d9ca7517edf468a
Tom Youngs <tomfyoungs@gmail.com>
2020-03-10 11:14:38 +0000

    save before vector implamentation

--- FollowMeApp/FollowMeApp/CanvasTool.cpp
+++ FollowMeApp/FollowMeApp/CanvasTool.cpp
@@ -11,8 +11,6 @@ CanvasTool::CanvasTool()
 CanvasTool::~CanvasTool()
 {
 }
- 
-
 
 void CanvasTool::onDraw() {
 
@@ -76,115 +74,109 @@ void CanvasTool::onLButtonDown(UINT nFlags, int x, int y)
 	}
 	 
 }
-
-
-
 void CanvasTool::startGame() {
 	
 	bool notGameOver = true;
-	int c_Origin = rand() % 4;//random number in range
-	int r_Origin = rand() % 4;
-	orderArr currentPathArr[20];//todo: vector?***********************
+	point OriginGen; 
+	 OriginGen.row = rand() % 4;//random number in range
+	 OriginGen.column = rand() % 4;
+	//orderArr currentPathArr[20];//todo: vector?***********************
+	vector<point> currentPathArr;
 	//pathsArr[r_Origin][c_Origin] = startTile;//set the origin
-	currentPathArr[0].row = r_Origin;
-	currentPathArr[0].column = c_Origin;
+	currentPathArr.push_back(OriginGen);
+	
 	for (int i = 0; i < (levelCounter+3); i++) {//loop through tile generator
 	
-		drawNewPath(r_Origin, c_Origin, currentPathArr, i);//code the next tile from the origin
+		drawNewPath(OriginGen, currentPathArr, i);//code the next tile from the origin
 	}
 	onDraw();
 	animatePathValid(currentPathArr);
 
 	levelCounter++;
 }
-
-void CanvasTool::drawNewPath(int& rO, int& cO, orderArr cpa[], int i) {
+void CanvasTool::drawNewPath(point& coords, vector<point> cpa, int i) {//todo: enum
+	
 	int tileChoice = rand() % 4; // chooses direction N=0, E=1, S=2, W=3
 	bool invalidRnd = true;
 	
 	//todo: can i make this more efficient/ compact?
 	while (invalidRnd) {//validation loop checks if the direction is out of bounds
-		if (cpa[i-1].row == cpa[i].row && cpa[i-1].column == cpa[i].column) {
-			tileChoice = rand() % 4;
+		if (cpa.size() > 1) {
+			if (cpa[i - 1].row == cpa[i].row && cpa[i - 1].column == cpa[i].column) {
+				tileChoice = rand() % 4;
+			}
 		}
-		else if ((rO - 1 < 0) && (tileChoice == 0)) {//if row -1 < 0 means that it is outside the bounds of the array so re-roll
-			tileChoice = rand() % 4;
+			else if ((coords.row - 1 < 0) && (tileChoice == 0)) {//if row -1 < 0 means that it is outside the bounds of the array so re-roll
+				tileChoice = rand() % 4;
+			}
+			else if ((coords.column + 1 > 3) && (tileChoice == 1)) {
+				tileChoice = rand() % 4;
+			}
+			else if ((coords.row + 1 > 3) && (tileChoice == 2)) {
+				tileChoice = rand() % 4;
+			}
+			else if ((coords.column - 1 < 0) && (tileChoice == 3)) {
+				tileChoice = rand() % 4;
+			}
+			else {
+				invalidRnd = false;
+			}
+		
 		}
-		else if((cO + 1 > 3) && (tileChoice == 1)){
-			tileChoice = rand() % 4;
-		}
-		else if ((rO + 1 > 3) && (tileChoice == 2)) {
-			tileChoice = rand() % 4;
-		}
-		else if ((cO - 1 < 0) && (tileChoice == 3)) {
-			tileChoice = rand() % 4;
-		}
-		else {
-			invalidRnd = false;
-		}
-	}
 	switch (tileChoice) {
 	case 0:
-		rO--;
-		pathsArr[rO][cO] = lightTile;
-		cpa[i].row = rO;
-		cpa[i].column = cO;
-
+		coords.row--;
+		pathsArr[coords.row][coords.column] = lightTile;
+		cpa.push_back(coords);
 		break;
 	case 1:
-		cO++;
-		pathsArr[rO][cO] = lightTile;
-		cpa[i].row = rO;
-		cpa[i].column = cO;
+		coords.column++;
+		pathsArr[coords.row][coords.column] = lightTile;
+		cpa.push_back(coords);
 		break;
 	case 2:
-		rO++;
-		pathsArr[rO][cO] = lightTile;
-		cpa[i].row = rO;
-		cpa[i].column = cO;
+		coords.row++;
+		pathsArr[coords.row][coords.column] = lightTile;
+		cpa.push_back(coords);
 		break;
 	case 3:
-		cO--;
-		pathsArr[rO][cO] = lightTile;
-		cpa[i].row = rO;
-		cpa[i].column = cO;
+		coords.column--;
+		pathsArr[coords.row][coords.column] = lightTile;
+		cpa.push_back(coords);
 		break;
 	}
 }
-
-
-void CanvasTool::animatePathValid(orderArr cpa[]) {
+void CanvasTool::animatePathValid(vector<point> cpa) {
 	//take in CPA[] and find the point to have the asset overlay for examble if the origin is at (2,1) the location
 	//of the asset would be (334,267) asset is 65x65 this assets x coord would incroment till it hits the next location
 	//minus 65 (334,334) minus 65 because the asset is tracked from the top left 
 	for (int i = 0; i < levelCounter + 3; i++) {//tile by tile
-
 		const int rowCalc = cpa[i + 1].row - cpa[i].row;
 		const int colCalc = cpa[i + 1].column - cpa[i].column;
 		if (rowCalc == 0) {
 			if (colCalc == -1) {
-				animatePath(cpa[i].row, cpa[i].column, rowCalc, colCalc);//west
+				animatePath(cpa[i], rowCalc, colCalc);//west
 			}
 			else if (colCalc == 1) {
-				animatePath(cpa[i].row, cpa[i].column, rowCalc, colCalc);//east
+				animatePath(cpa[i], rowCalc, colCalc);//east
 			}
 		}
 		else if (colCalc == 0) {
 			if (rowCalc == -1) {
-				animatePath(cpa[i].row, cpa[i].column, rowCalc, colCalc);//north
+				animatePath(cpa[i], rowCalc, colCalc);//north
 			}
 			else if (rowCalc == 1) {
-				animatePath(cpa[i].row, cpa[i].column, rowCalc, colCalc);//south
+				animatePath(cpa[i], rowCalc, colCalc);//south
 			}
 		}
 	}
 }
 
-void CanvasTool::animatePath(int row,int column, int N_S, int W_E) {
+void CanvasTool::animatePath(point coords, int N_S, int W_E) {
 		for (int j = 0; j < 65; j++) {
-			drawBitmap(balloonTile.c_str(), (column * 67 + 200) + j * W_E , (row * 67 + 100) + j * N_S, 65, 65);
+			drawBitmap(balloonTile.c_str(), (coords.column * 67 + 200) + j * W_E , (coords.row * 67 + 100) + j * N_S, 65, 65);
 			//todo: delay stuff, may be related to timer so could do that first maybe?
-			EasyGraphics::onDraw();
+			EasyGraphics::onDraw();//remove
 		}
 
 	//might need like a position counter in cpa
